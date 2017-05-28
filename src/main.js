dc668e4aa2694c29e98d7d1cf73cc6fa22063d02
maominfang <maominfang@meituan.com>
2017-05-28 15:46:20 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mmf on 17/5/28.
+ */
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import App from './App.vue'
+
+Vue.use(ElementUI)
+
+new Vue({
+        el: '#app',
+        render: h => h(App)
+})
