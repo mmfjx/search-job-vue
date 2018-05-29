0133a290cabdc3ebd77050be1dcfc1d90c9d4be5
elsia <elsiaperson@163.com>
2018-05-29 19:46:58 +0800

    config

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://awps-assets.meituan.net/hfe/hotel-fe-msc/',
+    assetsPublicPath: 'http://wuyushou.com/',
 
     /**
      * Source Maps
