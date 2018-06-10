a638c302b553caa2f00f12c1e0aade77cc94a8e9
elsia <elsiaperson@163.com>
2018-06-10 12:36:46 +0800

    condition

---
+++ src/components/condition.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="">
+        <mt-button >default</mt-button>
+        <mt-button >primary</mt-button>
+        <mt-button >danger</mt-button>
+        <mt-button >danger</mt-button>
+    </div>
+</template>
+
+<script>
+import { Button } from 'mint-ui';
+
+Vue.component(Button.name, Button);
+export default {
+    name: 'condition',
+    data () {
+        return {
+
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style>
+
+
+</style>
